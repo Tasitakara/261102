756485687b3e202591baba216ba7855b4bbc2ffc
Tasitakara <tasit_akara@cmu.ac.th>
2025-12-11 10:48:07 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int N;
+    int even=0;
+    int odd=0;
+    while(N!=0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        if(N==0){
+            break;
+        }
+        else{
+            if (N%2==0){
+                even ++;
+            }
+            if(N%2!=0){
+                odd ++;
+            }
+        }
+    }
+    cout << "#Even numbers = "<< even << endl;
+    cout << "#Odd numbers = "<< odd << endl;
     return 0;
 }
